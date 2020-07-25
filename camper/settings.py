ca22476adc1b43aab03ecff5d9cc459b486754e2
Vanessa Yoon <guswjd6005@gmail.com>
2020-07-25 16:53:27 +0900

    aaa

--- camper/settings.py
+++ camper/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'camper.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,4 +120,3 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-# aaaa
